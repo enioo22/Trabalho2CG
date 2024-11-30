f55f51f041c787ae87710a044473366146a7ef64
enio ferreira <e.nioferreira@hotmail.com>
2024-11-30 19:11:06 -0300

    Add Upgrades nao renovaveis e mais coisa pkrl

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -9,12 +9,18 @@ public class UIManager : MonoBehaviour
 {
 
     public GameObject PanelUpgrades;
+    public GameObject PanelUpgrades2;
     public Sprite CleanEnergySprite;
     public Sprite DirtyEnergySprite;
     public UnityEngine.UI.Button UpgradesShow;
     public UnityEngine.UI.Button UpgradeTypeChange;
     public TextMeshProUGUI UpgradeTypeText;
     public manager manager;
+    public GameObject Upgrade0;
+    public GameObject Upgrade1;
+
+    public UnityEngine.UI.Button Upgrades2Show;
+    public UnityEngine.UI.Button Upgrades2Close;
 
 
     public void onClickUpgrades()
@@ -22,9 +28,19 @@ public class UIManager : MonoBehaviour
         PanelUpgrades.SetActive(true);
     }
 
+    public void onClickUpgrades2()
+    {
+        PanelUpgrades2.SetActive(true);
+    }
+
     public void onClickCloseUpgrades()
     {
         PanelUpgrades.SetActive(false);
+    }
+
+    public void onClickCloseUpgrades2()
+    {
+        PanelUpgrades2.SetActive(false);
     }
 
     public void onClickChangeEnergyType()
--- Assets/manager.cs
+++ Assets/manager.cs
@@ -22,6 +22,15 @@ public class manager : MonoBehaviour
     public TextMeshProUGUI CO2EmissionText;
     public TextMeshProUGUI[] UpgradeButtonText;
 
+    public GameObject totalenergytext;
+    public GameObject powerperclick;
+    public GameObject powerpersecondtext;
+    public GameObject co2emissiontext;
+    public GameObject upgradesmenu1;
+    public GameObject upgradesmenu2;
+    public UnityEngine.UI.Button[] upgradeButton;
+    public GameObject[] upgrades;
+
     public float target;
 
 
@@ -58,41 +67,76 @@ public class manager : MonoBehaviour
         this.TotalClicks += this.PowerGenerationPerClick;
         this.PowerValue += this.PowerGenerationPerClick;
         this.TotalClicksText.text = Math.Round(this.TotalClicks, 2).ToString();
-        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.z, target, ref clickrotation, 0.1f);
-        if(PowerValue >= 10)
+        if(TotalClicks >= 1)
+        {
+            totalenergytext.SetActive(true);
+            powerperclick.SetActive(true);
+        }
+        if(TotalClicks == 10)
         {
             dialogmanager.callDialog(0);
+            upgradesmenu1.SetActive(true);
+
         }
-        transform.rotation = Quaternion.Euler(0, 0, angle);
+        if(TotalClicks > UpgradeCostRenovaveis[1])
+        {
+            upgrades[1].SetActive(true);
+        }
+        if(TotalClicks > UpgradeCostRenovaveis[2])
+        {
+            upgrades[2].SetActive(true);
+        }
+        if (TotalClicks > UpgradeCostRenovaveis[3])
+        {
+            upgrades[3].SetActive(true);
+        }
+        
     }
 
  // Upgrade para RenovÃ¡veis
     public void MakeUpgradeRenovavel(int position)
-    {   
+    {
+        powerpersecondtext.SetActive(true);
 
-        if (this.UpgradeCostRenovaveis[position] > this.TotalClicks)
-        {
-            Upgrade1ButtonText.color = Color.red; 
+        if(EnergyType == 0) {
+            if (this.UpgradeCostRenovaveis[position] > this.TotalClicks)
+            {
+                UpgradeButtonText[position].color = Color.red;
+                return;
+            }
+
+            ++this.UpgradeLevelRenovaveis[position];
+            double upgradelevel = this.UpgradeLevelRenovaveis[position];
+
+            if (upgradelevel >= 1 && upgradelevel <= 4)
+            {
+
+                GameObject.Find("/CanvasMain/Backgroundentities/Eolicos/Eolico" + upgradelevel).SetActive(true);
+                GameObject.Find("/CanvasMain/Backgroundentities/Eolicos/Eolico" + upgradelevel + "Pole").SetActive(true);
+
+            }
+            this.TotalClicks -= this.UpgradeCostRenovaveis[position];
+            if (UpgradeLevelRenovaveis[0] >= 25)
+            {
+                upgradesmenu2.SetActive(true);
+            }
+
+            this.UpgradeCostRenovaveis[position] *= 1.2;
+            this.PowerGenerationPerClick++;
+            this.TotalCO2Emission += CO2EmissionPerUpgradeRenovaveis[position];
             return;
         }
-        this.TotalClicks -= this.UpgradeCostRenovaveis[position];
-        this.UpgradeLevelRenovaveis[position]++;
-        this.UpgradeCostRenovaveis[position] *= 1.2;
-        this.PowerGenerationPerClick++;
-        this.TotalCO2Emission += CO2EmissionPerUpgradeRenovaveis[position];
-    }
-    // Upgrade para NÃ£o RenovÃ¡veis
-    public void MakeUpgradeNaoRenovavel(int position)
-    {
         if (this.UpgradeCostNaoRenovaveis[position] > this.TotalClicks)
         {
-            return; 
+            return;
         }
         this.TotalClicks -= this.UpgradeCostNaoRenovaveis[position];
         this.UpgradeLevelNaoRenovaveis[position]++;
         this.UpgradeCostNaoRenovaveis[position] *= 1.3;
         this.PowerGenerationPerClick += 2;
         this.TotalCO2Emission += CO2EmissionPerUpgradeNaoRenovaveis[position];
+
+
     }
     IEnumerator Waiter(float duration)
     {
@@ -112,11 +156,11 @@ public class manager : MonoBehaviour
     public String getUpgradeCost(int position)
     {
         return this.UpgradeCostRenovaveis[position].ToString();
+
     }
     
     private void Update()
     {
-
         this.TotalClicksText.text = Math.Round(this.TotalClicks, 2).ToString();
         this.PowerPerClick.text = this.PowerGenerationPerClick.ToString() + "MW/Click";
         this.CO2EmissionText.text = $"CO2 emitido: {Math.Round(this.TotalCO2Emission, 2)} KCO2";
@@ -124,14 +168,28 @@ public class manager : MonoBehaviour
         {
             case 0:
                 for (int i = 0; i < UpgradeRenovaveis.Length; i++)
-                {
+                {   
                     UpgradeButtonText[i].text = UpgradeRenovaveis[i] + "\nNível: " + (UpgradeLevelRenovaveis[i]) + "\nCusto: " + Math.Round(UpgradeCostRenovaveis[i], 2);
+                    if(TotalClicks < UpgradeCostRenovaveis[i])
+                    {
+                        upgradeButton[i].interactable = false;
+                    }
+                    UpgradeButtonText[i].color = Color.black;
+                    upgradeButton[i].interactable = true;
+
                 }
                 break;
             case 1:
                 for (int i = 0; i < UpgradeNaoRenovaveis.Length; i++)
                 {
                     UpgradeButtonText[i].text = UpgradeNaoRenovaveis[i] + "\nNível: " + (UpgradeLevelNaoRenovaveis[i]) + "\nCusto: " + Math.Round(UpgradeCostNaoRenovaveis[i], 2);
+
+                    if (TotalClicks < UpgradeCostNaoRenovaveis[i])
+                    {
+                        upgradeButton[i].interactable = false;
+                    }
+                    UpgradeButtonText[i].color = Color.black;
+                    upgradeButton[i].interactable = true;
                 }
                 break;
 
