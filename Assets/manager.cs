31f37f01ff6b27ade4a4ec9c7a0a248bd6487d91
enio ferreira <e.nioferreira@hotmail.com>
2024-12-01 18:23:32 -0300

    fix nao renovaveis, formatacao dos numeros, texto geracao passiva

--- Assets/manager.cs
+++ Assets/manager.cs
@@ -22,6 +22,8 @@ public class manager : MonoBehaviour
     public TextMeshProUGUI CO2EmissionText;
     public TextMeshProUGUI[] UpgradeButtonText;
 
+    public UnityEngine.UI.Button UpgradeVisual0, UpgradeVisual1; 
+
     public GameObject totalenergytext;
     public GameObject powerperclick;
     public GameObject powerpersecondtext;
@@ -34,8 +36,9 @@ public class manager : MonoBehaviour
     public float target;
 
 
-    float PowerValue;
-    float PowerGenerationPerClick;
+    double PowerValue = 0;
+    double PowerGenerationPerClick;
+    double PowerPerClickEolica = 0, PowerPerClickHidro = 0, PowerPerClickBio = 0, PowerPerClickSolar = 0, PowerPerClickGas = 0, PowerPerClickOleo = 0, PowerPerClickCarvao=0, PowerPerClickNuke = 0;
     float PowerGenerationPerSecond;
     double TotalClicks;
     double TotalCO2Emission;
@@ -55,6 +58,10 @@ public class manager : MonoBehaviour
     double[] UpgradeCostNaoRenovaveis = { 500, 2000, 5000, 1000000 };
     double[] UpgradeLevelNaoRenovaveis = { 0, 0, 0, 0 };
     double[] CO2EmissionPerUpgradeNaoRenovaveis = { 486, 840, 1000, 13 }; // Emissão em KCO₂/MW/h
+
+    double upgradeVisualEolico1, upgradeVisualEolico2;
+
+    
     public double getUpgradeCost(double InitialCost, int NumberOfUpgrades)
     {
         return InitialCost * Math.Pow(1.2, NumberOfUpgrades);
@@ -65,6 +72,7 @@ public class manager : MonoBehaviour
     public void mainClick()
     {
         this.TotalClicks += this.PowerGenerationPerClick;
+        //this.TotalClicks += 100000;
         this.PowerValue += this.PowerGenerationPerClick;
         this.TotalClicksText.text = Math.Round(this.TotalClicks, 2).ToString();
         if(TotalClicks >= 1)
@@ -121,63 +129,105 @@ public class manager : MonoBehaviour
                 upgradesmenu2.SetActive(true);
             }
 
-            this.UpgradeCostRenovaveis[position] *= 1.2;
-            this.PowerGenerationPerClick++;
+            this.UpgradeCostRenovaveis[position] *= 1.1;
             this.TotalCO2Emission += CO2EmissionPerUpgradeRenovaveis[position];
+            PowerPerClickEolica = UpgradeLevelRenovaveis[0] * 0.5 * (1 + upgradeVisualEolico1 + upgradeVisualEolico2);
+            PowerPerClickHidro = UpgradeLevelRenovaveis[1] * 5;
+            PowerPerClickBio = UpgradeLevelRenovaveis[2] * 25;
+            PowerPerClickSolar = UpgradeLevelRenovaveis[3] * 100;
+
+            PowerPerClickGas = UpgradeLevelNaoRenovaveis[0] * 2;
+            PowerPerClickOleo = UpgradeLevelNaoRenovaveis[1] * 10;
+            PowerPerClickCarvao = UpgradeLevelNaoRenovaveis[2] * 15;
+            PowerPerClickNuke = UpgradeLevelNaoRenovaveis[3] * 50;
+
+
+
             return;
         }
-        if (this.UpgradeCostRenovaveis[position] > this.TotalClicks)
+        if (this.UpgradeCostNaoRenovaveis[position] > this.TotalClicks)
         {
             Upgrade1ButtonText.color = Color.red; 
             return;
         }
         this.TotalClicks -= this.UpgradeCostRenovaveis[position];
-        this.UpgradeLevelRenovaveis[position]++;
-        this.UpgradeCostRenovaveis[position] *= 1.2;
+        this.UpgradeLevelNaoRenovaveis[position]++;
+        this.UpgradeCostNaoRenovaveis[position] *= 1.15;
         this.PowerGenerationPerClick++;
-        this.TotalCO2Emission += CO2EmissionPerUpgradeRenovaveis[position];
+        this.TotalCO2Emission += CO2EmissionPerUpgradeNaoRenovaveis[position];
     }
     // Upgrade para Não Renováveis
-    public void MakeUpgradeNaoRenovavel(int position)
+    public void upgradeVisual(int upgrade)
     {
-        if (this.UpgradeCostNaoRenovaveis[position] > this.TotalClicks)
+        Debug.Log(upgrade);
+        switch (upgrade)
         {
-            return;
-        }
-        this.TotalClicks -= this.UpgradeCostNaoRenovaveis[position];
-        this.UpgradeLevelNaoRenovaveis[position]++;
-        this.UpgradeCostNaoRenovaveis[position] *= 1.3;
-        this.PowerGenerationPerClick += 2;
-        this.TotalCO2Emission += CO2EmissionPerUpgradeNaoRenovaveis[position];
+            case 0:
 
+                upgradeVisualEolico1 = 2;
+                TotalClicks = TotalClicks - 10000;
+                break;
+            case 1:
+                TotalClicks -= 1000000;
+                upgradeVisualEolico2 = 5;
+                break;
+        }
 
     }
-    IEnumerator Waiter(float duration)
+    string GetNumeroFormatado( double numero)
     {
-        Debug.Log($"Started at {Time.time}, waiting for {duration} seconds");
-        yield return new WaitForSeconds(duration);
-        Debug.Log($"Ended at {Time.time}");
+
+        if (numero > 10000)
+        {
+            int power = (int)Math.Floor(Math.Log10(numero) + 1);
+            if (power >= 5 && power <7)
+            {
+                string returnstring = "";
+                
+                returnstring = Math.Round((numero / 1000),3).ToString();
+                returnstring += "k";
+                return returnstring;
+            }
+            if(power >= 7)
+            {
+                string returnstring = "";
+                returnstring = Math.Round((numero / 1000000), 3).ToString();
+                returnstring += "M";
+                return returnstring;
+
+            }
+        }
+        return Math.Round(numero, 2).ToString();
+
     }
     // Start is called before the first frame update
     void Start() 
     {
         this.TotalClicks = 0;
-        this.PowerGenerationPerSecond = this.PowerValue = 0;
+        this.PowerGenerationPerSecond = 0;
         this.PowerGenerationPerClick = 1;
         this.TotalCO2Emission = 0;
     }
+
     
     private void Update()
     {
-        this.TotalClicksText.text = Math.Round(this.TotalClicks, 2).ToString();
-        this.PowerPerClick.text = this.PowerGenerationPerClick.ToString() + "MW/Click";
+        this.TotalClicksText.text = GetNumeroFormatado(TotalClicks);
+        PowerGenerationPerClick = 1 + PowerPerClickEolica + PowerPerClickHidro + PowerPerClickBio + PowerPerClickSolar + PowerPerClickGas +PowerPerClickCarvao + PowerPerClickOleo + PowerPerClickNuke;
+        PowerGenerationPerSecondText.text = GetNumeroFormatado(0.1 * PowerGenerationPerClick).ToString();
+        this.PowerPerClick.text = GetNumeroFormatado(PowerGenerationPerClick) + "MW/Click";
         this.CO2EmissionText.text = $"CO2 emitido: {Math.Round(this.TotalCO2Emission, 2)} KGCO2";
+
+        UpgradeVisual0.interactable = TotalClicks > 10000;
+
+        UpgradeVisual1.interactable = TotalClicks >= 1000000 && UpgradeLevelRenovaveis[0] >= 100;
+
         switch (EnergyType)
         {
             case 0:
                 for (int i = 0; i < UpgradeRenovaveis.Length; i++)
                 {
-                    UpgradeButtonText[i].text = UpgradeRenovaveis[i] + "\nNível: " + (UpgradeLevelRenovaveis[i]) + "\nCusto: " + Math.Round(UpgradeCostRenovaveis[i], 2);
+                    UpgradeButtonText[i].text = UpgradeRenovaveis[i] + "\nNível: " + (UpgradeLevelRenovaveis[i]) + "\nCusto: " + GetNumeroFormatado(UpgradeCostRenovaveis[i]);
                     if(TotalClicks < UpgradeCostRenovaveis[i])
                     {
                         upgradeButton[i].interactable = false;
@@ -186,11 +236,14 @@ public class manager : MonoBehaviour
                     upgradeButton[i].interactable = true;
 
                 }
-                    break;
+                
+                
+
+                break;
             case 1:
                 for (int i = 0; i < UpgradeNaoRenovaveis.Length; i++)
                 {
-                    UpgradeButtonText[i].text = UpgradeNaoRenovaveis[i] + "\nNível: " + UpgradeLevelNaoRenovaveis[i] + "\nCusto: " + Math.Round(UpgradeCostNaoRenovaveis[i], 2);
+                    UpgradeButtonText[i].text = UpgradeNaoRenovaveis[i] + "\nNível: " + UpgradeLevelNaoRenovaveis[i] + "\nCusto: " + GetNumeroFormatado(UpgradeCostNaoRenovaveis[i]);
 
                     if (TotalClicks < UpgradeCostNaoRenovaveis[i])
                     {
